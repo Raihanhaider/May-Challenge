183580a2c671f3f352104d3a65db5c582986fbe4
Rehan Haider <37216398+Raihanhaider@users.noreply.github.com>
2020-05-05 15:12:52 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,17 @@
+//First Non-Repeating character in a string
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        int alpha[26]={0};
+        
+        for(int i=0;i<s.size();i++)
+            alpha[(s[i]-'a')]++;
+        for(int i=0;i<s.size();i++)
+            if(alpha[s[i]-'a']==1)
+                return i;
+        return -1;
+        
+    }
+};
